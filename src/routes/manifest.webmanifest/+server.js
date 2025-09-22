fa73497d6ff264e1497848d5fffcf8b052a78180
Anthony Ettinger <anthony@chovy.com>
2025-09-21 22:26:55 -0700

    Remove webmanifest redirect, restore PWA install prompt functionality

--- src/routes/manifest.webmanifest/+server.js
+++
@@ -1,9 +0,0 @@
-// Redirect legacy .webmanifest requests to .json
-export async function GET() {
-	return new Response(null, {
-		status: 301,
-		headers: {
-			'Location': '/manifest.json'
-		}
-	});
-}
