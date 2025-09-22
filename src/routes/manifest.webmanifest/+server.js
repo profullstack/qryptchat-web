7cb8fcb3aba73e0813e28a7515c5b675c9335599
Anthony Ettinger <anthony@chovy.com>
2025-09-21 22:57:16 -0700

    maskable icon

---
+++ src/routes/manifest.webmanifest/+server.js
@@ -0,0 +1,67 @@
+export async function GET() {
+  return new Response(
+    JSON.stringify({
+      name: "QryptChat - Quantum-Resistant Messaging",
+      short_name: "QryptChat",
+      description: "Secure, quantum-resistant end-to-end encrypted messaging",
+      start_url: "/",
+      scope: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      theme_color: "#6366f1",
+      icons: [
+        {
+          src: "/icons/android-chrome-36x36.png",
+          sizes: "36x36",
+          type: "image/png"
+        },
+        {
+          src: "/icons/android-chrome-48x48.png",
+          sizes: "48x48",
+          type: "image/png"
+        },
+        {
+          src: "/icons/android-chrome-72x72.png",
+          sizes: "72x72",
+          type: "image/png"
+        },
+        {
+          src: "/icons/android-chrome-96x96.png",
+          sizes: "96x96",
+          type: "image/png"
+        },
+        {
+          src: "/icons/android-chrome-144x144.png",
+          sizes: "144x144",
+          type: "image/png"
+        },
+        {
+          src: "/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "/icons/android-chrome-256x256.png",
+          sizes: "256x256",
+          type: "image/png"
+        },
+        {
+          src: "/icons/android-chrome-384x384.png",
+          sizes: "384x384",
+          type: "image/png"
+        },
+        {
+          src: "/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable any"
+        }
+      ]
+    }),
+    {
+      headers: {
+        "Content-Type": "application/manifest+json"
+      }
+    }
+  );
+}
